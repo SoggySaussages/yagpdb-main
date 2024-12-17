7079682717b94d340a7d3e92f0502d84082afd4c
SoggySaussages <vmdmaharaj@gmail.com>
2024-12-17 02:53:13 -0800

    genai: add template funcs
    Signed-off-by: SoggySaussages <vmdmaharaj@gmail.com>

---
+++ genai/tmplextensions.go
@@ -0,0 +1,108 @@
+package genai
+
+import (
+	"encoding/json"
+
+	"github.com/botlabs-gg/yagpdb/v2/common/templates"
+	"github.com/botlabs-gg/yagpdb/v2/lib/dstate"
+)
+
+func init() {
+	templates.RegisterSetupFunc(func(ctx *templates.Context) {
+		ctx.ContextFuncs["genaiComplete"] = tmplGenAIComplete(ctx)
+
+		ctx.ContextFuncs["genaiComplete"] = tmplGenAIComplete(ctx)
+		ctx.ContextFuncs["genaiCompleteComplex"] = tmplGenAICompleteComplex(ctx)
+		ctx.ContextFuncs["genaiModerate"] = tmplGenAIModerate(ctx)
+	})
+}
+
+func CreateGenAIInput(values ...interface{}) (*GenAIInput, error) {
+	if len(values) < 1 {
+		return &GenAIInput{}, nil
+	}
+
+	var m map[string]interface{}
+	switch t := values[0].(type) {
+	case templates.SDict:
+		m = t
+	case *templates.SDict:
+		m = *t
+	case map[string]interface{}:
+		m = t
+	case *GenAIInput:
+		return t, nil
+	default:
+		dict, err := templates.StringKeyDictionary(values...)
+		if err != nil {
+			return nil, err
+		}
+		m = dict
+	}
+
+	encoded, err := json.Marshal(m)
+	if err != nil {
+		return nil, err
+	}
+
+	var input *GenAIInput
+	err = json.Unmarshal(encoded, &input)
+	if err != nil {
+		return nil, err
+	}
+
+	return input, nil
+}
+
+func tmplGenAIComplete(ctx *templates.Context) interface{} {
+	return func(systemMsg, userMsg string, maxTokens int64) (string, error) {
+		if ctx.IncreaseCheckGenericAPICall() {
+			return "", templates.ErrTooManyAPICalls
+		}
+
+		config, err := GetConfig(ctx.GS.ID)
+		if err != nil {
+			return "", err
+		}
+
+		provider := GenAIProviderFromID(config.Provider)
+		response, _, err := provider.BasicCompletion(&dstate.GuildState{ID: ctx.GS.ID, OwnerID: ctx.GS.OwnerID}, systemMsg, userMsg, maxTokens, false)
+
+		return response.Content, err
+	}
+}
+
+func tmplGenAICompleteComplex(ctx *templates.Context) interface{} {
+	return func(input *GenAIInput) (*GenAIResponse, *GenAIResponseUsage, error) {
+		if ctx.IncreaseCheckGenericAPICall() {
+			return nil, nil, templates.ErrTooManyAPICalls
+		}
+
+		config, err := GetConfig(ctx.GS.ID)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		provider := GenAIProviderFromID(config.Provider)
+		input.BotSystemMessage = BotSystemMessagePromptGeneric + "\n" + BotSystemMessagePromptAppendNonNSFW
+		return provider.ComplexCompletion(&dstate.GuildState{ID: ctx.GS.ID, OwnerID: ctx.GS.OwnerID}, input)
+	}
+}
+
+func tmplGenAIModerate(ctx *templates.Context) interface{} {
+	return func(message string) (*GenAIModerationCategoryProbability, error) {
+		if ctx.IncreaseCheckGenericAPICall() {
+			return nil, templates.ErrTooManyAPICalls
+		}
+
+		config, err := GetConfig(ctx.GS.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		provider := GenAIProviderFromID(config.Provider)
+		response, _, err := provider.ModerateMessage(&dstate.GuildState{ID: ctx.GS.ID, OwnerID: ctx.GS.OwnerID}, message)
+
+		return response, err
+	}
+}
