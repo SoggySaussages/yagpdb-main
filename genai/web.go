3b5b364325768047d3ca10babbbd1b9d4500baca
SoggySaussages <vmdmaharaj@gmail.com>
2024-12-19 01:49:50 -0800

    genai: fix bad import
    Signed-off-by: SoggySaussages <vmdmaharaj@gmail.com>

--- genai/web.go
+++ genai/web.go
@@ -18,8 +18,8 @@ import (
 	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dstate"
 	"github.com/botlabs-gg/yagpdb/v2/web"
-	"github.com/volatiletech/sqlboiler/queries/qm"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"goji.io"
 	"goji.io/pat"
 )
