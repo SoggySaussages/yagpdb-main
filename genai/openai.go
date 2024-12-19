55148dab0a8b74f42ea3f877f17e7dbab827b1c9
SoggySaussages <vmdmaharaj@gmail.com>
2024-12-18 23:28:29 -0800

    openai: tweak generation settings
    Signed-off-by: SoggySaussages <vmdmaharaj@gmail.com>

--- genai/openai.go
+++ genai/openai.go
@@ -146,7 +146,7 @@ func (p GenAIProviderOpenAI) ComplexCompletion(gs *dstate.GuildState, input *Gen
 		}
 	}
 
-	requestParams := openai.ChatCompletionNewParams{Model: openai.F(config.Model), MaxCompletionTokens: openai.Int(input.MaxTokens)}
+	requestParams := openai.ChatCompletionNewParams{Model: openai.F(config.Model), MaxCompletionTokens: openai.Int(input.MaxTokens), Temperature: openai.Float(1.1), PresencePenalty: openai.Float(0.1)}
 
 	if len(messages) > 0 {
 		requestParams.Messages = openai.F(messages)
