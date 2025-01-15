d5edc3a9bab94901e935080c9d9b08c7aa18a20d
SoggySaussages <vmdmaharaj@gmail.com>
2025-01-15 00:15:14 -0800

    genai/google: switch back to latest models
    Signed-off-by: SoggySaussages <vmdmaharaj@gmail.com>

--- genai/google.go
+++ genai/google.go
@@ -30,10 +30,10 @@ func (p GenAIProviderGoogle) DefaultModel() string {
 }
 
 var GenAIModelMapGoogle = &GenAIProviderModelMap{
-	"Gemini 1.0 Pro":                  "gemini-1.0-pro",
-	"Gemini 1.5 Pro":                  "gemini-1.5-pro",
-	"Gemini 1.5 Flash":                "gemini-1.5-flash",
-	"Gemini 2.0 Flash (experimental)": "gemini-2.0-flash-exp",
+	"Gemini 1.0 Pro":                  "gemini-1.0-pro-latest",
+	"Gemini 1.5 Pro":                  "gemini-1.5-pro-latest",
+	"Gemini 1.5 Flash":                "gemini-1.5-flash-latest",
+	"Gemini 2.0 Flash (experimental)": "gemini-2.0-flash-exp-latest",
 }
 
 func (p GenAIProviderGoogle) ModelMap() *GenAIProviderModelMap {
